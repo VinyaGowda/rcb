2eb2284edef64626ad64bbe603d405efb0a5b9e0
%newname% <%newname%@Vinya>
2025-12-03 15:15:06 +0530

    First commit

---
+++ src/test/java/github/GitHubPracticeTest.java
@@ -0,0 +1,14 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitHubPracticeTest {
+	@Test
+	public void gitHubPrcaticeTest() {
+		Reporter.log("hii github here", true);
+		//String BROWSER = System.getProperty("browser");
+		//System.out.println(BROWSER);
+	}
+
+}
